46701c3967d3838659aaa33193e7863f1bfd9bb8
Edward9686 <ink.sans00@mail.ru>
2024-12-25 21:21:03 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -10,15 +11,15 @@ async def home_page():
 
 @app.get('/user/admin')
 async def admin():
-    return {'Вы вошли как администор'}
+    return 'Вы вошли как администор'
 
 
 @app.get('/user/{user_id}')
 async def user_id(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='1')):
-    return {f'Вы вошли как пользователь № {user_id}'}
+    return f'Вы вошли как пользователь № {user_id}.'
 
 
 @app.get('/user/{username}/{age}')
-async def admin(username: str = Path(ge=5, le=20, description='Enter username', example='UrbanUser'),
-                age: int = Path(ge=18, le=120, description='Enter age', example='24')):
-    return {f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def user_info(username: Annotated[str, Path(ge=5, le=20, description='Enter username', example='UrbanUser')],
+                    age: int = Path(ge=18, le=120, description='Enter age', example='24')):
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}.'
